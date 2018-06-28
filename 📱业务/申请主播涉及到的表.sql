e43b712b8e83bd7d0e6e6185ebecf9fc68996315
shengjian <531522023@qq.com>
2018-06-28 17:07:33 +0800

    fix

---
+++ 📱业务/申请主播涉及到的表.sql
@@ -0,0 +1,33 @@
+oracle:
+
+  SELECT *
+  FROM ACTOR_BANK_ACCOUNT WHERE ACTORID = 10003986;
+
+  SELECT *
+  FROM FAMILY_MEMBER WHERE USERID = 10003986;
+
+  SELECT *
+  FROM ACTOR_INFO WHERE ACTORID = 10003986;
+
+  SELECT *
+  FROM ROOM_INFO WHERE ROOMID = 10003986;
+
+  SELECT *
+  FROM CHANNELUSER WHERE CHANNELID = 10003986;
+
+pg tshow:
+
+  select *
+  from hist_zmrz_apply;
+
+  SELECT * 
+  FROM res_user_verify;
+
+  SELECT *
+  FROM res_user_apply_actor;
+
+  SELECT *
+  FROM res_room_info;
+
+  select *
+  from res_room_weight where subcatlg_id = 26;
